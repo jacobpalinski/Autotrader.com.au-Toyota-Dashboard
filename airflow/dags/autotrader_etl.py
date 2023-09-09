8b61c4b7c733affa8f73c5a6b16ed103eb6fdbc8
root <root@LAPTOP-2Q577UFQ>
2023-09-09 13:42:54 +0800

    create + insert australian_suburbs table

--- airflow/dags/autotrader_etl.py
+++ airflow/dags/autotrader_etl.py
@@ -35,6 +35,21 @@ with DAG(dag_id = 'Autotrader_ETL_DAG', default_args = default_args, schedule_in
         ],
         gcp_conn_id = 'google_cloud'
     )
+    create_australian_suburbs_table = BigQueryCreateEmptyTableOperator(
+        task_id = 'create_australian_suburbs_table',
+        dataset_id = 'autotrader_staging',
+        table_id = 'australian_suburbs',
+        project_id = 'autotrader-toyota-dashboard',
+        schema_fields = [
+            {'name': 'id', 'type': 'INT64'},
+            {'name': 'suburb', 'type': 'STRING'},
+            {'name': 'state', 'type': 'STRING'},
+            {'name': 'latitude', 'type': 'FLOAT64'},
+            {'name': 'longitude', 'type': 'FLOAT64'}
+        ],
+        gcp_conn_id = 'google_cloud'
+    )
+    empty1 = EmptyOperator(task_id = 'empty1')
     insert_raw_data_into_listings_raw = GCSToBigQueryOperator(
         task_id = 'insert_raw_data_into_listings_raw',
         bucket = 'autotrader-raw',
@@ -50,6 +65,22 @@ with DAG(dag_id = 'Autotrader_ETL_DAG', default_args = default_args, schedule_in
             {'name': 'state', 'type': 'STRING'}
         ],
         source_format = 'NEWLINE_DELIMITED_JSON',
+        write_disposition = 'WRITE_TRUNCATE',
+        gcp_conn_id = 'google_cloud'
+    )
+    insert_csv_data_into_australian_suburbs_table = GCSToBigQueryOperator(
+        task_id = 'insert_csv_data_into_australian_suburbs_table',
+        bucket = 'autotrader-raw',
+        source_objects = 'australian_suburbs.csv',
+        destination_project_dataset_table = 'autotrader-toyota-dashboard.autotrader_staging.australian_suburbs',
+        schema_fields = [
+            {'name': 'id', 'type': 'INT64'},
+            {'name': 'suburb', 'type': 'STRING'},
+            {'name': 'state', 'type': 'STRING'},
+            {'name': 'latitude', 'type': 'FLOAT64'},
+            {'name': 'longitude', 'type': 'FLOAT64'}
+        ],
+        source_format = 'CSV',
         write_disposition = 'WRITE_TRUNCATE',
         gcp_conn_id = 'google_cloud'
     )
@@ -182,7 +213,7 @@ with DAG(dag_id = 'Autotrader_ETL_DAG', default_args = default_args, schedule_in
         gcp_conn_id = 'google_cloud',
         use_legacy_sql = False
     )
-    empty = EmptyOperator(task_id = 'empty')
+    empty2 = EmptyOperator(task_id = 'empty2')
     insert_into_car_dim = BigQueryExecuteQueryOperator(
         task_id = 'insert_into_car_dim',
         sql = '''INSERT INTO autotrader-toyota-dashboard.autotrader_transformed.car_dim (car_key, year, car_model, type)
@@ -260,9 +291,9 @@ with DAG(dag_id = 'Autotrader_ETL_DAG', default_args = default_args, schedule_in
         use_legacy_sql = False
     )
     
-    extract >> create_listings_raw_table >> insert_raw_data_into_listings_raw >> remove_nulls \
-    >> uppercase_columns >> format_columns >> string_to_int >> [create_car_dim, create_location_dim] \
-    >> empty >> [insert_into_car_dim, insert_into_location_dim] >> create_listing_fact >> insert_into_listing_fact
+    extract >> [create_listings_raw_table, create_australian_suburbs_table] >> empty1 >> [insert_raw_data_into_listings_raw, insert_csv_data_into_australian_suburbs_table] \
+    >> remove_nulls >> uppercase_columns >> format_columns >> string_to_int >> [create_car_dim, create_location_dim] \
+    >> empty2 >> [insert_into_car_dim, insert_into_location_dim] >> create_listing_fact >> insert_into_listing_fact
     
 
 
